a6827fd6bd0e062ae1f5b0cf0ba15aa1fc74e149
Mona Lisa <joannagrosshans2@gmail.com>
2021-01-28 19:21:18 -0600

    wrapper deleted

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Card from 'react-bootstrap/Card';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Col, Row, CardGroup
@@ -11,17 +10,6 @@ import Portfolio from '../Portfolio';
 
 function ProjectCard(projects) {
     return (
-        // <Card className="bg-dark text-white">
-        //     <Card.Img style= {{ height: "250px"}} src="../../assets/Portfolio/0.png" alt="Card image" />
-        //      <Card.ImgOverlay>
-        //         <Card.Title>Tally Max Word Generator</Card.Title>
-        //         <Card.Text>
-        //             This is a wider card with supporting text below as a natural lead-in to
-        //              additional content. This content is a little bit longer.
-        //          </Card.Text>
-        //         <Card.Text>Last updated 3 mins ago</Card.Text>
-        //     </Card.ImgOverlay>
-        // </Card>
         <div>
         <Row md={3}>
         {/* <Col md={4}> */}
