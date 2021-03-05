7fe1428feae46101ae80895d8078d39414cbfdd8
Mona Lisa <joannagrosshans2@gmail.com>
2021-03-05 15:25:00 -0600

    about changed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,18 +3,21 @@ import image from 'react-bootstrap/Image';
 
 function About() {
     return (
-      <section style={{ padding: "20px"}}>
+      <section style={{padding: "20px"}}>
         <h1 style={{margin: "auto"}} id="about">ABOUT JOANNA</h1>
-        <div style={{display: "inline-block", padding: "20px"}}>
+        <div style={{display: "flex", padding: "20px"}}>
             <div>
-                <img
-                style= {{width: "45%", justifyContent: "center"}}
+                <img 
+                // style={{maxwidth: "40%"}}
                 src={require(`../../assets/About/0.jpg`).default} rounded="true"
                 alt="Joanna Grosshans"
                 key="Joanna Grosshans"
                 />
             </div>
-            <div style={{ fontWeight: "bold", fontSize: "20px", margin: "150px, 150px, 150px, 150px"}}>
+            <div 
+            // style={{ fontWeight: "bold", fontSize: "20px", margin: "150px, 150px, 150px, 150px"}}
+            
+            style={{display: "flex", border: "solid black 3px "}}>
             Welcome! Coming from years of performing, teaching, and working on my skills as a professional violinist, my focus has shifted from creating music with other people to creating web applications. I am interested in working with others to build useful and meaningful projects while enjoying the problem solving and creative side of web development. I enjoy working with and creating databases, and using that data, as well as constantly learning as a path to write quality code. I am excited to utilize my skills as a problem solver and effective time management as part of a quality-driven team in a fast-paced environment. I earned a certificate from University of Wisconsin in Full Stack Web Developer in February 2021.
             In my free time I can be found spending time with my family, and baking up something sweet! Have a look around my site, and contact me for any additional information!
           </div>
--- src/components/NavTabs/index.js
+++ src/components/NavTabs/index.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function NavTabs(props) {
   const tabs = ['About', 'Contact', 'Resume', 'Portfolio'];
   return (
+    // <ul className="nav nav-tabs justify-content-center">
+    //   {tabs.map(tab => (
+    //     <li className="nav-item" key={tab}>
+    //       <a   style={{ color: "black", fontSize: "20px"}}
+    //         href={'#' + tab.toLowerCase()}
+    //         onClick={() => props.handlePageChange(tab)}
+    //         className={
+    //           props.currentPage === tab ? 'nav-link active' : 'nav-link'
+    //         }
+    //       >
+    //         {tab}
+    //       </a>
+    //     </li>
+    //   ))}
+    // </ul>
+    <div>
     <ul className="nav nav-tabs justify-content-center">
-      {tabs.map(tab => (
-        <li className="nav-item" key={tab}>
-          <a   style={{ color: "black", fontSize: "20px"}}
-            href={'#' + tab.toLowerCase()}
-            onClick={() => props.handlePageChange(tab)}
-            className={
-              props.currentPage === tab ? 'nav-link active' : 'nav-link'
-            }
-          >
-            {tab}
-          </a>
-        </li>
-      ))}
+      <li className="nav-item" style={{ margin: "10px"}}>
+        <a href="/">Home</a>
+      </li>
+      <li className="nav-item" style={{ margin: "10px"}}>
+        <a href="/About">About</a>
+      </li>
+      <li style={{ margin: "10px"}}>
+        <a href="/Contact">Contact</a>
+      </li>
+      <li style={{ margin: "10px"}}>
+        <a href="/Portfolio">Portfolio</a>
+      </li>
     </ul>
+  </div>
   );
 }
 
