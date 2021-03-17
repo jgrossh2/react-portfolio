4622ddca6587f606856e2eadee6a7311019bfd9f
Mona Lisa <joannagrosshans2@gmail.com>
2021-03-17 14:53:41 -0500

    portfolio arranged

--- src/components/PortfolioInfo/index.js
+++ src/components/PortfolioInfo/index.js
@@ -1,44 +1,48 @@
-import React, { useState } from 'react';
-import projects from '../../projects.json'
-import ProjectCard from '../ProjectCard';
+import React, { useState } from "react";
+import projects from "../../projects.json";
+import ProjectCard from "../ProjectCard";
 
-const PortfolioInfo = ()  => {
-   
-    const [currentProject, setCurrentProject] =useState(projects);
+const PortfolioInfo = () => {
+  const [currentProject, setCurrentProject] = useState(projects);
 
-    return(
-        <div style={{padding: "20px"}}>
-            <div>
-                <h2>PORTFOLIO</h2>
-            </div>
-            
-            <div>
-                <div
-                // style={{flexWrap: "wrap", width: "100%", maxWidth: "500px", margin: "30px" }}
-                // style={{ position: "relative", padding: "100px" }}
-                // style={{ display:"flex"}}
-                style={{ flexWrap: "wrap", display: "flex", marginLeft: "200px", cursor: "pointer"}}
-                // style={{width: "auto", height: "300px", marginBottom: "50px", cursor: "pointer"}}
-                >
-                   {projects.map((projects, i) => (
-                       <ProjectCard
-                        image={require(`../../assets/Portfolio/${i}.png`).default}
-                        alt={projects.name}
-                        // className='img-thumbnail mx-1 flex-row'
-                        key={projects.name}
-                        name={projects.name}
-                        description={projects.description}
-                        part={projects.part}
-                        group={projects.group}
-                        Github={projects.Github}
-                        deployed={projects.deployed}
-                        /> 
-                   ))}
-                </div>
-            </div>
-         
+  return (
+    <div style={{ padding: "20px" }}>
+      <div>
+        <h2>PORTFOLIO</h2>
+      </div>
+
+      <div
+        className="projects"
+        style={{
+          width: "100%",
+          margin: "0 auto",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        <div
+          className="project"
+          style={{ flexWrap: "wrap", display: "flex", marginLeft: "200px" }}
+        >
+          {projects.map((projects, i) => (
+            <ProjectCard
+              image={require(`../../assets/Portfolio/${i}.png`).default}
+              alt={projects.name}
+              className="img-thumbnail mx-1"
+              key={projects.name}
+              name={projects.name}
+              description={projects.description}
+              part={projects.part}
+              group={projects.group}
+              Github={projects.Github}
+              deployed={projects.deployed}
+            />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PortfolioInfo;
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,48 +1,29 @@
 import React from "react";
-// import {
-//     Card, CardImg, CardText, CardBody,
-//     CardTitle, CardSubtitle, CardGroup
-//   } from 'reactstrap';
 import tally from "../../assets/Portfolio/0.png";
 import projects from "../../../src/projects.json";
 import Portfolio from "../PortfolioInfo";
 
 function ProjectCard(props) {
   return (
-    <div
-    // style={{flexWrap: "wrap", width: "100%", maxWidth: "500px", margin: "30px" }}
-    // style={{ width: "75%", }}
-    // style={{ position: "relative", width: "100%", maxWidth: "500px", margin: "30px" }}
-    >
-      {/* <CardGroup
-        // style={{ width: "50%"}}
-        > */}
-      {/* <Card  */}
-      <div
-        style={{ margin: "20px", height: "250px", width: "500px"}}
-        // style={{margin: "auto", background: "none", display: "flex", maxWidth: "50%", padding: "20px"}}
-      >
-        <div class="img-square-wrapper">
-          <img
-            // style={{ height: "300px", border: "solid 1px", margin: "30px"}}
-            // style={{width: "100%", minHeight: "300px", backgroundSize: "cover", display: "flex", padding: "15px", position: "relative", overflow: "hidden"}}
-            src={props.image}
-            alt={props.name}
-          />
+    <div>
+      <div style={{ margin: "20px", width: "500px" }}>
+        <img style={{ width: "500px"}} src={props.image} alt={props.name} />
+        <div style={{ padding: "10px", border: "solid 1px grey"}}>
+          <h6>{props.name}</h6>
+          <h6>{props.description}</h6>
+          <p>{props.part}</p>
+          <p>{props.group}</p>
+          <p>
+            <a href={props.Github} target="_blank" rel="noreferrer">
+              {props.Github}
+            </a>
+          </p>
+          <p>
+            <a href={props.deployed} target="_blank" rel="noreferrer">
+              {props.deployed}
+            </a>
+          </p>
         </div>
-        {/* <div className="border" style={{ margin: "20px"}}>
-            <h5 tag="h5">{props.name}</h5>
-            <h6 tag="h6" 
-            // className="mb-2 text-muted"
-            >{props.description}</h6>
-            <div>
-            <p>{props.part}</p>
-            <p>{props.group}</p>
-            <p><a href={props.Github} target="_blank" rel='noreferrer'>{props.Github}</a></p>
-            <p><a href={props.deployed} target="_blank" rel='noreferrer'>{props.deployed}</a></p>
-            </div> */}
-
-        {/* </div> */}
       </div>
     </div>
   );
